746c38802368406c5a57ef1b2a343a3926229c4a
Oleg Sharapa <sharapa.oleg@gmail.com>
2020-03-11 22:27:09 +0300

    added Error page for incorrect routes

---
+++ src/Views/404.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import { Button } from "react-bootstrap";
+
+export default function ErrorPage() {
+  const history = useHistory();
+  return (
+    <div className="error-page-container">
+      <img
+        className="error-img"
+        src={process.env.PUBLIC_URL + "/404.png"}
+        alt="error img"
+      />
+      <div
+        style={{
+          position: "absolute",
+          left: "50%",
+          margin: "-50px 0 0 -80px"
+        }}
+      >
+        <Button
+          style={{ width: "70px", marginRight: "20px" }}
+          onClick={() => history.goBack()}
+        >
+          Back
+        </Button>
+        <Link to="/">
+          <Button style={{ width: "70px" }}>Home</Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
